35cbe78fa7db0c338108a4355a3495d6b810201f
Matthew Strong <mast4878@colorado.edu>
2020-10-13 10:22:37 -0600

    results!

--- job.sh
+++ job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --ntasks=12
-#SBATCH --time=06-23
+#SBATCH --ntasks=6
+#SBATCH --time=02-23
 #SBATCH --partition=shas
 #SBATCH --qos=long
 #SBATCH --output=sample-%j.out
